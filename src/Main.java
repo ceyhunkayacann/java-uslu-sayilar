cfbfc403f4ac53bacc86ab61a6262da95f55da79
ceyhunkayacan <ceyslab@gmail.com>
2022-12-02 14:53:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n,k;
+        int total = 1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı : ");
+        n = input.nextInt();
+
+        System.out.print("Üs sayısı : ");
+        k = input.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            total *= n;
+        }
+
+        System.out.println("Sonuç : " + total);
+
+    }
+}
